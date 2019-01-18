846322481e5b353516e075d110a1940220e91ad6
Arturo <arturoborbolla.95@hotmail.com>
2019-01-18 07:05:39 -0500

    FC

--- run.py
+++ run.py
@@ -6,9 +6,9 @@ for line in fileinput.input():
 	lines.append(line)
 
 if any("." in s for s in lines):
-	lines[0:]= [float(x) for x in sys.argv[0:]]
+	lines[0:]= [float(x) for x in lines[0:]]
 else:
-	lines[0:]= [int(x) for x in sys.argv[0:]]
+	lines[0:]= [int(x) for x in lines[0:]]
 
 
 suma = 0
